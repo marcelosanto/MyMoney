fe786d2b6d0b4ff48f1dcacc48fb7c7a1206c4f8
Marcelo Santos <rocha.mer21@gmail.com>
2019-09-02 10:36:36 -0300

    criado o hooks de deletar os dados por id

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import useGet from './useGet'
 import usePost from './usePost'
+import useDelete from './useDelete'
 
 
 const url = 'https://mymoney-10de2.firebaseio.com/movimentacoes/2019-08.json'
@@ -9,20 +10,25 @@ const url = 'https://mymoney-10de2.firebaseio.com/movimentacoes/2019-08.json'
 function App() {
   const data = useGet(url)
   const [postData, post] = usePost(url)
+  const [deleteData, remove] = useDelete()
   
 
   const saveNew = () => {
     post({ valor: 25, descricao: 'funfou mano' })
   }
 
+  const doRemove = () => {
+    remove('https://mymoney-10de2.firebaseio.com/movimentacoes/2019-08/a.json')
+  }
+
   return (
     <div>
-
       <h1>My Money</h1>
       { JSON.stringify(data) }
       { data.loading && <p>Loading...</p> }
       <button onClick={saveNew}>Salvar</button>
       <pre>{JSON.stringify(postData)}</pre>
+      <button onClick={doRemove}>Deletar</button>
 
     </div>
   )
