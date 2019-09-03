36341282c20c7e4500622b4389b23ec5094ea0e5
Marcelo Santos <rocha.mer21@gmail.com>
2019-09-03 20:19:07 -0300

    add botstrap e comecando a componentizar o MyMoney

--- src/App.js
+++ src/App.js
@@ -5,27 +5,76 @@ const baseUrl = 'https://mymoney-10de2.firebaseio.com/'
 const { useGet, usePost, useDelete } = Rest(baseUrl)
 
 function App() {
-  const data = useGet('movimentacoes/2019-08')
-  const [postData, post] = usePost('movimentacoes/2019-08')
-  const [deleteData, remove] = useDelete()
-  
-  const saveNew = () => {
-    post({ valor: 25, descricao: 'funfou mano' })
-  }
+  const data = useGet('meses')
+  //const [postData, post] = usePost('movimentacoes/2019-08')
+  //const [deleteData, remove] = useDelete()
 
-  const doRemove = () => {
-    remove('movimentacoes/2019-08/-LnsNK4V9k3f3PBzNsN-')
-  }
+  // const saveNew = () => {
+  //   post({ valor: 25, descricao: 'funfou mano' })
+  // }
+
+  // const doRemove = () => {
+  //   remove('movimentacoes/2019-08/-LnsNK4V9k3f3PBzNsN-')
+  // }
 
   return (
     <div>
-      <h1>My Money</h1>
-      { JSON.stringify(data) }
-      { data.loading && <p>Loading...</p> }
-      <button onClick={saveNew}>Salvar</button>
-      <pre>{JSON.stringify(postData)}</pre>
-      <button onClick={doRemove}>Deletar</button>
+      <nav className='navbar navbar-light bg-light'>
+        <div className='container'>
+          <a className='navbar-brand'>MyMoney</a>
+        </div>
 
+      </nav>
+      <div className='container'>
+      <h2>Adicionar mês</h2>
+      <select>
+        <option value='2019'>2019</option>
+        <option value='2020'>2020</option>
+      </select>
+      <select>
+        <option value='01'>01</option>
+        <option value='02'>02</option>
+      </select>
+      <button>Adicionar mês</button>
+      {
+        data.loading && <span>Carregando...</span>
+      }
+      {
+        !data.loading && (
+          <table className='table'>
+            <thead>
+              <tr>
+                <th>Mês</th>
+                <th>Previsão entrada</th>
+                <th>Entrada</th>
+                <th>Previsão saída</th>
+                <th>Saída</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                Object
+                  .keys(data.data)
+                  .map(mes => {
+                    return (
+                      <tr key={mes}>
+                        <td>{mes}</td>
+                        <td>{data.data[mes].previsao_entrada}</td>
+                        <td>{data.data[mes].entradas}</td>
+                        <td>{data.data[mes].previsao_saida}</td>
+                        <td>{data.data[mes].saidas}</td>
+                      </tr>
+                    )
+                  })
+              }
+
+            </tbody>
+          </table>
+        )
+      }
+
+      <pre>{JSON.stringify(data)}</pre>
+      </div>
     </div>
   )
 }
